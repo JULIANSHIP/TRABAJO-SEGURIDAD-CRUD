6244fbb6978b64a3a00ed126209b56dfad6622d9
JULIANSHIP <85257286+JULIANSHIP@users.noreply.github.com>
2021-08-20 19:02:20 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import formulario from './Formulario';
+import React, { Component } from 'react'
+
+
+
+function App() {
+  return (
+      <div className="app">
+        <formulario/>
+
+        
+        
+    </div>
+  )
+}
+
+export default App;
---
+++ formulario.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+function formulario(){
+    return (<div></div>
+       
+    )
+    
+}
+export default formulario;
